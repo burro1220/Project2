1273e49f87478aedd7a192987edddf7d7d9aecb7
Blake Burroughs <b.burro@gmail.com>
2019-02-19 16:16:59 -0600

    Completed pagination and linking, working on active links

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Project-2
+//
+
+***
+This is a basic readme to meet project requirements for Treehouse Project-2
+a project on DOM manipulation.
+
+This project required the creation of 
+
+I am going for "Exceeds Requirements"
+
+This project was created by Blake Burroughs.
+***
--- js/script.js
+++ js/script.js
@@ -41,15 +41,68 @@ function showPage(list, page) {
   };
 };
 
-showPage(studentsList, 6);
+
 
 /***
    Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
+function appendPageLinks(list) {
+  // determine number of pages needed
+  const numPages = Math.ceil(list.length/10);
+  // set node reference for appending div
+  const page = document.querySelector('div.page');
+  //create new div to hold pagination numbers
+  const div = document.createElement('div');
+  //set div className to Pagination
+  div.className = "pagination";
+  //append the new div to page reference
+  page.appendChild(div);
+  //create and append ul to pagination div
+  const ul = document.createElement('ul');
+  div.appendChild(ul);
+  //loop through and create/append list items and links
+  for(i = 1; i <= numPages; i++){
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = i + ' ';
+    li.appendChild(a);
+    ul.appendChild(li)
+  }
+};
+
+
+//setup for initial view
+function pageSetup(list) {
+  showPage(list, 1);
+  appendPageLinks(list);
+};
+window.onload = pageSetup(studentsList);
+
+
+// remove active links from pagination and add active to target
+//credit https://stackoverflow.com/questions/38990163/how-can-i-add-and-remove-an-active-class-to-an-element-in-pure-javascript/38990288
+function removeActive() {
+  var el = document.querySelectorAll(".active");
+    if(el){
+    el.classList.remove('.active')
+  } else {
+    console.log('no el');
+  }
+
+};
 
 
 
+//set click listener on a tags
+aTags = document.querySelectorAll('a');
+for(let i = 1; i < aTags.length; i++) {
+  aTags[i].addEventListener('click', (e) => {
+    showPage(studentsList, i);
+    removeActive();
+    e.target.className = 'active';
 
+  });
+}
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
